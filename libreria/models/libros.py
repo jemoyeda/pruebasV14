efc7ffad0731e8bcfe8f994d8aea9d00457dea53
Ing. Hermes Colina <79210037+HermesColina2909@users.noreply.github.com>
2022-05-03 10:46:55 -0400

    libreria_3

--- libreria/models/libros.py
+++ libreria/models/libros.py
@@ -1,3 +1,4 @@
+from tkinter import commondialog
 from odoo import models, fields
 
 #creando un modelo a partir de una clase
@@ -7,6 +8,8 @@ class Libros(models.Model):
     name = fields.Char(string="Nombre del libro", required=True)
     editorial = fields.Char(string="editorial", required=True)
     isbn = fields.Char(string="ISBN", required=True)
+    autor_id = fields.Many2one(comodel_name='autor', string="Autor", required=True)
+    
     
     
     
